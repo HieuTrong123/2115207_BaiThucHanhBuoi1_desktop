4ce3fc420dc948bfb0fb884e5fbabf84ed854360
HieuTrong123 <87361973+HieuTrong123@users.noreply.github.com>
2023-03-10 22:55:51 +0700

    bt

--- 2115207_DinhTrongHieu_Lab1/2115207_DinhTrongHieu_Lab1/frmBai4.Designer.cs
+++ 2115207_DinhTrongHieu_Lab1/2115207_DinhTrongHieu_Lab1/frmBai4.Designer.cs
@@ -30,9 +30,10 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNhap = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.lblKQ = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -53,12 +54,12 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "nhập số cần tìm";
             // 
-            // textBox1
+            // txtNhap
             // 
-            this.textBox1.Location = new System.Drawing.Point(478, 86);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(97, 26);
-            this.textBox1.TabIndex = 2;
+            this.txtNhap.Location = new System.Drawing.Point(478, 86);
+            this.txtNhap.Name = "txtNhap";
+            this.txtNhap.Size = new System.Drawing.Size(97, 26);
+            this.txtNhap.TabIndex = 2;
             // 
             // button1
             // 
@@ -68,6 +69,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Tìm số";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
@@ -80,18 +82,30 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "kết quả";
             // 
+            // lblKQ
+            // 
+            this.lblKQ.AutoSize = true;
+            this.lblKQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKQ.ForeColor = System.Drawing.Color.Red;
+            this.lblKQ.Location = new System.Drawing.Point(490, 290);
+            this.lblKQ.Name = "lblKQ";
+            this.lblKQ.Size = new System.Drawing.Size(0, 29);
+            this.lblKQ.TabIndex = 5;
+            // 
             // frmBai4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKQ);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtNhap);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Name = "frmBai4";
             this.Text = "frmBai4";
+            this.Load += new System.EventHandler(this.frmBai4_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,8 +115,9 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNhap;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblKQ;
     }
 }
--- 2115207_DinhTrongHieu_Lab1/2115207_DinhTrongHieu_Lab1/frmBai4.cs
+++ 2115207_DinhTrongHieu_Lab1/2115207_DinhTrongHieu_Lab1/frmBai4.cs
@@ -16,5 +16,33 @@ namespace _2115207_DinhTrongHieu_Lab1
         {
             InitializeComponent();
         }
+
+        private void frmBai4_Load(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int so;
+            for (int i = 0; i <= 10; i++)
+            {
+                so = rand.Next(1, 100);
+                listBox1.Items.Add(so);
+            }
+        }
+        bool TimKiem(int so)
+        {
+            foreach (int item in listBox1.Items)
+            {
+                if(item==so)
+                    return true;
+            }
+            return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var so = int.Parse(txtNhap.Text);
+            if (TimKiem(so))
+                lblKQ.Text = "Tìm Thấy";
+            else
+                lblKQ.Text = "Không Tìm Thấy";
+        }
     }
 }
